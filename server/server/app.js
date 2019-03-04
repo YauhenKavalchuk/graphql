1f21a3219a0a61de0c10f0eda10e63def8dfa5be
Yauhen <silent_control@tut.by>
2019-03-04 20:02:36 +0300

    Lesson 01: Setup Environment

---
+++ server/server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+const PORT = 3005;
+
+app.use('/graphql', graphqlHTTP({}));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
