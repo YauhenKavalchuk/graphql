4311dc77d5a3ae9d8ab70d8bdeeba5d18c2f9295
Yauhen <silent_control@tut.by>
2019-03-05 21:18:58 +0300

    Lesson 03: The Resolve Function & Testing Query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,18 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+const movies = [
+	{ id: '1', name: 'Pulp Fiction', genre: 'Crime' },
+	{ id: '2', name: '1984', genre: 'Sci-Fi' },
+	{ id: 3, name: 'V for vendetta', genre: 'Sci-Fi-Triller' },
+	{ id: 4, name: 'Snatch', genre: 'Crime-Comedy' },
+];
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -16,9 +23,9 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-
+        return movies.find(movie => movie.id == args.id);
       },
     },
   }
