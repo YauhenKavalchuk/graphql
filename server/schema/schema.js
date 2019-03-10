00b6e3f161856bcf290eb1f02a8b2654d8c86316
Yauhen <silent_control@tut.by>
2019-03-10 18:59:45 +0300

    Lesson 08: Create Mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -78,6 +78,42 @@ const DirectorType = new GraphQLObjectType({
   }),
 });
 
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addDirector: {
+			type: DirectorType,
+			args: {
+				name: { type: GraphQLString },
+				age: { type: GraphQLInt },
+			},
+			resolve(parent, args) {
+				const director = new Directors({
+					name: args.name,
+					age: args.age,
+				});
+				return director.save();
+			},
+		},
+		addMovie: {
+			type: MovieType,
+			args: {
+				name: { type: GraphQLString },
+				genre: { type: GraphQLString },
+				directorId: { type: GraphQLID },
+			},
+			resolve(parent, args) {
+				const movie = new Movies({
+					name: args.name,
+					genre: args.genre,
+					directorId: args.directorId,
+				});
+				return movie.save();
+			},
+		}
+	}
+});
+
 const Query = new GraphQLObjectType({
   name: 'Query',
   fields: {
@@ -116,4 +152,5 @@ const Query = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: Query,
+	mutation: Mutation,
 });
